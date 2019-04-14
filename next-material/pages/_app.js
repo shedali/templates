a068cc8df1e16c34a024bba360c9f6e282344564
Franz <shedali@gmail.com>
2019-04-14 22:23:51 +0100

    monorepo run all the things

--- next-material/pages/_app.js
+++ next-material/pages/_app.js
@@ -25,7 +25,7 @@ class MyApp extends App {
     return (
       <Container>
         <Head>
-          <title>My page</title>
+          <title>Next Material</title>
         </Head>
         {/* Wrap every page in Jss and Theme providers */}
         <JssProvider
